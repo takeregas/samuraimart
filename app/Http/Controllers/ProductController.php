d97009489dbd5055f59fe3ba6163422b02a7ae3a
“takeregas” <takeregas.chelsea@gmail.com>
2022-10-03 23:21:54 +0000

    09 create reviews

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -58,7 +58,9 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        return view('products.show', compact('product'));
+        $reviews = $product->reviews()->get();
+ 
+        return view('products.show', compact('product', 'reviews'));
     }
 
     /**
