e1fbd521975d85d91dc0c2ae0f88af9f742d1386
“takeregas” <takeregas.chelsea@gmail.com>
2022-10-16 10:56:42 +0000

    16

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ Route::get('/', function () {
   Route::get('users/mypage/edit', 'UserController@edit')->name('mypage.edit');
   Route::get('users/mypage/address/edit', 'UserController@edit_address')->name('mypage.edit_address');
   Route::put('users/mypage', 'UserController@update')->name('mypage.update');
+  Route::get('users/mypage/password/edit', 'UserController@edit_password')->name('mypage.edit_password');
+  Route::put('users/mypage/password', 'UserController@update_password')->name('mypage.update_password');
+
 
 Route::post('products/{product}/reviews', 'ReviewController@store');
  
